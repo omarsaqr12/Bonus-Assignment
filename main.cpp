02dff769994ea8819e7ccd1794d22d6e518a9c2e
omarsaqr12 <125351767+omarsaqr12@users.noreply.github.com>
2026-09-18 14:58:18 +0300

    Use standard C++ entry point and explicit graph filename

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
- #include"Graphs.h"
- #include"Edge.h"
- #include"Graphs.cpp"
-int main() {
- Graphs gra;
- gra.getGraph("TestG");
+#include "Graphs.h"
 
-gra.dispGraph();
-gra.dispEdges();
-gra.shPath(0);
+#include <exception>
+#include <iostream>
+#include <string>
 
+int main(int argc, char* argv[]) {
+    const std::string path = argc > 1 ? argv[1] : "TestG.csv";
+    try {
+        Graphs graph;
+        graph.getGraph(path);
+        std::cout << "Vertices: " << graph.No_of_Verices()
+                  << ", edges: " << graph.No_of_Edges() << '\n';
+        graph.dispGraph();
+        graph.dispEdges();
+        graph.DFS();
+        graph.shPath(0);
+    } catch (const std::exception& error) {
+        std::cerr << "Graph error: " << error.what() << '\n';
+        return 1;
+    }
 }
- 
